c2edeeb6eee68ed06b1e41048b0b5bf17e47e34d
edmcorpuz <152798215+edmcorpuz@users.noreply.github.com>
2025-03-17 20:30:23 +0800

    Update search_sort.py
    added  quick sort and the first part of def main() for displaying available columns, user input for column and target and choosing search method.

--- search_sort.py
+++ search_sort.py
@@ -67,49 +67,55 @@ class DiabetesData(SearchSortBase):
     sorted_data[column] = values
     end_time = time.time()
     return sorted_data, end_time - start_time
-    
 
+  def quick_sort(self, column):
+    sorted_data = self.df.copy()
+    values = sorted_data[column].tolist()
+    start_time = time.time()
 
+    def quicksort(arr):
+      if len(arr) <=1:
+        return arr
+      pivot = arr[len(arr) // 2]
+      left = [x for x in arr if x < pivot]
+      middle = [x for x in arr if x == pivot]
+      right = [x for x in arr if x > pivot]
+      return quicksort(left) + middle + quicksort(right)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    sorted_vallues = quicksort(values)
+    sorted_data[column] = sorted_values
+    end_timme = time.time()
+    return sorted_data, end_time - start_time
 
 def main():
-  #part ni bloodmonke
+  diabetes = DiabetesData()
 
+  # Display available columns
+  print("Available columns:")
+  print(diabetes.df.columns.tolist())
 
+  # User input for column and target
+  column = input("Choose a column to search (Glucose, Age. BMI, etc.): ")
+  target = input("Enter a value to search for: ")
+
+  # Choose search method
+  search_method = input("\nChoose search method:\n1. Linear Search\n2. Binary Search\nEnter choice: ")
+  if search_method == "1":
+    start_time = time.time()
+    result = diabetes.linear_search(column, target)
+    end_time = time.time()
+    print(f"Linear Search: {'Found at row index' + str(result) if result != -1 else 'Not Found'}")
+    print(f"Time taken: {end-time - start_time:.6f}s")
+  elif search_method == "2":
+    print(f"\nSorting \"{column}\" column before Binary Search...")
+    sorted_df, _ = diabetes.quick_sort(column)
+    start_time = time.time()
+    result = diabetes.binary_search(column, target)
+    end_time = time.time()
+    print(f"Binary Search: {'Found at row index' + srt(result if result != -1 else 'Not Found'}")
+    print(f"Time taken: {end_time - start_time:.6f}s")
+  else:
+    print("Invalid choice for search method.\n")
 
   # User input fo the sorting part
   sort_column = input("\nChoose a column to sort (Glucose, Age, BMI, etc.): ")
