4a9fa498cc94b7cce966bfefea4ad06ec0fef8a7
AZFaberes <audrey6905@gmail.com>
2025-03-17 19:12:35 +0800

    Update search_sort.py

--- search_sort.py
+++ search_sort.py
@@ -22,16 +22,16 @@ class SearchSortBase:
 
 #----Bubble, Selection, and Insertion Sort----
 class DiabetesData(SearchSortBase):
-def bubble_sort(self, column):
-  sorted_data = self.df.copy()
-  values = sorted_data[column].tolist()
-  start_time = time.time()
+  def bubble_sort(self, column):
+    sorted_data = self.df.copy()
+    values = sorted_data[column].tolist()
+    start_time = time.time()
 
-n = len(values)
-for i in range (n):
-  for j in range(0, n - i - 1):
-    if values[j] > valurd[j + 1]:
-      values[j], values[j+1] = values[j + 1], values[j]
-sorted_data[column] = values
-end_time = time.time()
-return sorted_data, end_time - start_time
+  n = len(values)
+  for i in range (n):
+    for j in range(0, n - i - 1):
+      if values[j] > valurd[j + 1]:
+        values[j], values[j+1] = values[j + 1], values[j]
+  sorted_data[column] = values
+  end_time = time.time()
+  return sorted_data, end_time - start_time
