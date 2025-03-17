0a5e3e1884166c5b6e456c318b8f84fe5fb67b26
z-sulit <150438758+z-sulit@users.noreply.github.com>
2025-03-17 13:25:34 +0200

    Update search_sort.py

--- search_sort.py
+++ search_sort.py
@@ -35,3 +35,38 @@ class DiabetesData(SearchSortBase):
   sorted_data[column] = values
   end_time = time.time()
   return sorted_data, end_time - start_time
+
+
+def main():
+  #part ni bloodmonke
+
+
+
+  # User input fo the sorting part
+  sort_column = input("\nChoose a column to sort (Glucose, Age, BMI, etc.): ")
+  sort_algorithm = input("Choose sorting algorithm:\n1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n4. Quick Sort\nEnter choice: \n")
+
+  if sort_algorithm == "1":
+    print(f"\nSorting by \"{sort_column}\" using Bubble Sort...")
+    sorted_df, time_taken = diabetes.bubble_sort(sort_column)
+  elif sort_algorithm == "2":
+    print(f"\\Sorting By \"{sort_column}\" using Selection Sort...")
+    sorted_df, time_taken = diabetes.selection_sort(sort_column)
+  elif sort_algorithm == "3":
+    print(f"\nSorting by \"{sort_column}\" using Insertion Sort...")
+    sorted_df, time_taken = diabetes.insertion_sort(sort_column)
+  elif  sort_algorithm == "4":
+    print(f"\nSorting by \"{sort_column}\" using Quick Sort...")
+    sorted_df, time_taken = diabetes.quick_sort(sort_column)
+
+  else:
+    print("Invalid choice.....")
+    return
+
+  pritn("Sorting completed...")
+  diabetes.save_sorted_data(sorted_df)
+  print(f"Time taken: {time_taken:.6f}s")
+
+if __name__ == "__main__":
+  main()
+  
