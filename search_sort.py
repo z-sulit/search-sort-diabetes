13fdf43f54c47215d30383acfd77741803adb42e
z-sulit <150438758+z-sulit@users.noreply.github.com>
2025-03-17 13:09:37 +0200

    Update search_sort.py

--- search_sort.py
+++ search_sort.py
@@ -1,13 +1,33 @@
 import pandas as pd
+import time
+
+class SearchSortBase:
+  def __init__(self, file_path="diabetes.csv"):
+    self.file_path = file_path
+    self.df = self.load_csv()
+
+  def load_csv(self):
+    try:
+      df = pd.read_csv(self.file_path)
+      print("Dataset loaded!!")
+      return df
+    except FileNotFoundError:
+      print("Error: File not found.....")
+      return None
+
+  def save_sorted_data(self, sorted_df, filename="sorted_diabetes.csv"):
+    sorted_df.to_csv(filemame, index=False)
+    print(f"Sorted data saved to \"{filename}\".")
+      
 
 #----Bubble, Selection, and Insertion Sort----
-
+class DiabetesData(SearchSortBase):
 def bubble_sort(self, column):
   sorted_data = self.df.copy()
   values = sorted_data[column].tolist()
   start_time = time.time()
 
-n = len(alues)
+n = len(values)
 for i in range (n):
   for j in range(0, n - i - 1):
     if values[j] > valurd[j + 1]:
